d84073acb527feab99ba73c0c3f11b04617471d6
Vasyly Belokopytov <44782827+vasyokbelokopytov@users.noreply.github.com>
2020-04-19 15:11:45 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -11,7 +11,7 @@ CREATE TABLE Bands (
     ,formed_year NUMBER(4,0) NOT NULL
     ,split_year NUMBER(4,0) 
     ,country_name VARCHAR(20) NOT NULL REFERENCES Countries(country_name)
-    ,fans NUMBER(4,0) NOT NULL
+    ,fans NUMBER(4,0) NOT NULL CHECK(fans >= 0)
     ,CONSTRAINT CHK_Years CHECK (formed_year > 0 AND split_year > 0)
     ,CONSTRAINT PK_Bands PRIMARY KEY (band_name, formed_year, country_name)
 );
