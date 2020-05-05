88f2d4bcfa7baecdbb4d74728165a1a24df7c89b
Vasyly Belokopytov <44782827+vasyokbelokopytov@users.noreply.github.com>
2020-05-05 09:02:06 +0300

    Create export.py

---
+++ laboratory 3/export.py
@@ -0,0 +1,36 @@
+import csv
+import cx_Oracle
+
+username = 'SYSTEM'
+password = '123'
+dsn = 'localhost/xe'
+
+connection = cx_Oracle.connect(username, password, dsn)
+
+tables = ['Bands', 'Genres', 'Countries', 'Bands_Genres', 'Fans']
+
+for table in tables:
+    with open('C:/Users/admin/Desktop/' + table + '.csv', 'w') as file:
+        writer = csv.writer(file, delimiter=',')
+
+        query = "SELECT * FROM " + table
+
+        cursor = connection.cursor()
+        cursor.execute(query)
+
+        content = []
+        column_names = []
+
+        for name in cursor.description:
+            column_names.append(name[0])
+
+        writer.writerow(column_names)
+
+        for row in cursor:
+            content.append(list(row))
+
+        for row in content:
+            writer.writerow(row)
+
+        cursor.close()
+
