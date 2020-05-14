afa2900b16be43ac0cdf863f44828104d76c5915
Vasyly Belokopytov <44782827+vasyokbelokopytov@users.noreply.github.com>
2020-05-14 18:50:55 +0300

    Update populate.sql

--- laboratory 4/populate.sql
+++ laboratory 4/populate.sql
@@ -1,12 +1,12 @@
 BEGIN
     for i in 1..3 loop
     
-        INSERT INTO Countries (country_name) VALUES ('COUNTRY' || i);
-        INSERT INTO Genres (genre_name) VALUES ('GENRE' || i);
-        INSERT INTO Fans (fan_name, birth_date) VALUES ('FAN' || i, ADD_MONTHS(SYSDATE(), -i*123));
-        INSERT INTO Bands (band_name, formed_year, country_name, split_year) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, 1980 + 6*i);
-        INSERT INTO Bands_Genres (band_name, formed_year, country_name, genre_name) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, 'GENRE' || i);
-        INSERT INTO Bands_Fans (band_name, formed_year, country_name, fan_name, birth_date) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, 'FAN' || i, ADD_MONTHS(SYSDATE(), -i*123));
+        INSERT INTO Country (country_name) VALUES ('COUNTRY' || i);
+        INSERT INTO Genre (genre_name) VALUES ('GENRE' || i);
+        INSERT INTO Fan (id, fan_name) VALUES (i, 'FAN' || i);
+        INSERT INTO Band (band_name, formed_year, country_name, split_year) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, 1980 + 6*i);
+        INSERT INTO Band_Genre (band_name, formed_year, country_name, genre_name) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, 'GENRE' || i);
+        INSERT INTO Band_Fan (band_name, formed_year, country_name, id) VALUES ('BAND' || i, 1980 + i, 'COUNTRY' || i, i);
         
     end loop;
 END;
